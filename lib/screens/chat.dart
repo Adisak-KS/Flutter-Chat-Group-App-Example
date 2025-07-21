ff64ed723b78465e56a35eb5669e773b3e00b74f
Adisak-KS <Adisak.General@gmail.com>
2025-07-21 10:45:21 +0700

    first commit

---
+++ lib/screens/chat.dart
@@ -0,0 +1,61 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class ChartScreen extends StatefulWidget {
+  const ChartScreen({super.key});
+
+  @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+
+    await fcm.requestPermission();
+
+    fcm.subscribeToTopic('chat');
+
+
+    final token = await fcm.getToken();
+    print(
+      token,
+    ); // your could send this token (via HTTP or the Firestore SDK) to a backend
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter Chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(
+              Icons.exit_to_app,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: const [
+          Expanded(child: ChatMessages()),
+          NewMessage(),
+        ],
+      ),
+    );
+  }
+}
